85c5140c4fbad0b18408e12ad3f28310ce468bf0
Elana Kopelevich <ekopelevich@gmail.com>
2017-03-25 19:44:24 -0600

    Initial Commit after cloning template

--- README.md
+++ README.md
@@ -1,14 +1,8 @@
-# angular-minimum-webpack-example
-_A bare minimum Angular setup using webpack 2_
+# DenverScript Angular Unit Testing Example
 
-## Why
+Built on top of [angular-minimum-webpack-example](https://github.com/msarsha/angular-minimum-webpack-example)
 
-Wanted to learn and understand how to setup an Angular project using webpack, but every starter or seed out there was too overwhelming and contain a lot of bloat for someone who just started with webpack.
-
-So hopefuly others in the same situation as me could find this example helpful.
-
-
-### How to use
+## How to use
 1. Clone
 2. Install - `yarn install`
 3. Build - `yarn run build`
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
-import { Component } from "@angular/core";
+import { Component } from "@angular/core"
 
 @Component({
   selector: 'app-root',
   template: `
     <h1 class="app">{{title}}</h1>
+    <div *ngIf="speakers">
+      <h2>Speakers</h2>
+      <ul>
+        <li *ngFor="let speaker of speakers">{{speaker}}</li>
+      </ul>
+    </div>
   `,
   styles: [
-    `.app{
-      background-color: #f4f4f4;
+    `
+    .app {
+      color: #333333;
+      font-family: 'Helvetica', sans-serif;
     }
     `
   ]
 })
+
 export class AppComponent {
-  title: String = "App Works !";
+  title: string = "Hello DenverScript!"
+  speakers: string[] = ["Emma"]
 }
